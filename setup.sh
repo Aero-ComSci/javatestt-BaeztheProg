92623a4f4a52177cfdd4213093acdc9d35f24640
Mr. Baez <baezlopezolsen@dublinusd.org>
2026-01-30 20:02:12 -0800

    Updated dependencies

--- setup.sh
+++ setup.sh
@@ -5,6 +5,9 @@ sourceSets {
     main { java { srcDirs = ['.'] } }
     test { java { srcDirs = ['.'] } }
 }
-dependencies { testImplementation 'org.junit.jupiter:junit-jupiter:5.10.0' }
+dependencies { // Add JUnit 5 Jupiter API & Engine for testing 
+ testImplementation'org.junit.jupiter:junit-jupiter-api:5.9.3'
+ testRuntimeOnly 'org.junit.jupiter:junit-jupiter-engine:5.9.3' }
+
 test { useJUnitPlatform() }
 EOF

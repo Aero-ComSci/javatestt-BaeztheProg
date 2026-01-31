bebce3444ee545de5078857a97ba3efb2fbb5bcb
Mr. Olsen Miguel Baez <148258174+BaeztheProg@users.noreply.github.com>
2026-01-30 21:31:26 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,7 +11,10 @@ plugins { id 'java' }
 
 repositories { mavenCentral() }
 
-dependencies { testImplementation 'org.junit.jupiter:junit-jupiter:5.10.0' }
+dependencies { 
+testImplementation 'org.junit.jupiter:junit-jupiter:5.10.1'
+testRuntimeOnly 'org.junit.platform:junit-platform-launcher:1.10.1'
+}
 
 test { useJUnitPlatform() } 
 EOF
